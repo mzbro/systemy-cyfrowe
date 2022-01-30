18ee64e78c44fc007540e43433d21a3b212a668f
Michał Zbroszczyk <mzbroszczyk@gmail.com>
2022-01-30 19:44:03 +0100

    Poprawiono generację białych znaków

--- src/LogisimTestVectorGenerator.cpp
+++ src/LogisimTestVectorGenerator.cpp
@@ -12,7 +12,7 @@
 using std::cout;
 using std::endl;
 
-constexpr static unsigned short bit_res    = 4;
+constexpr static unsigned short bit_res = 4;
 constexpr static unsigned short input_size = 7;
 
 int main() {
@@ -27,7 +27,7 @@ int main() {
             test_file << (char) ('a' + i) << "[" << bit_res << "]";
         else
             test_file << (char) (122 - 2 * input_size + i + 1) << "[" << bit_res << "]";
-        if (i < 2 * input_size - 2)
+        if (i < 2 * input_size - 1)
             test_file << " ";
     }
     test_file << endl;
@@ -35,9 +35,7 @@ int main() {
         std::vector<unsigned short> vData;
         for (unsigned short i = 0; i < input_size; ++i) {
             vData.push_back(rand() % static_cast<unsigned int>(pow(2, bit_res)));
-            test_file << std::bitset<bit_res>(vData[vData.size() - 1]);
-            if (i != input_size - 1)
-                test_file << " ";
+            test_file << std::bitset<bit_res>(vData[vData.size() - 1]) << " ";
         }
         sort(vData.begin(), vData.end());
         for (unsigned short i = 0; i < input_size; ++i) {
